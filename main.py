2c6dd576fd7558515b73fdaddfd933ac12411698
jkkang94 <45805490+jkkang94@users.noreply.github.com>
2024-02-22 08:36:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import streamlit as st
-# 텍스트 출력
-st.text('이것은 텍스트 출력입니다.')
+from PIL import Image
+
+# 사진 업로드 위젯
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    # PIL로 이미지를 열기
+    image = Image.open(uploaded_file)
+    
+    # 이미지 표시
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
